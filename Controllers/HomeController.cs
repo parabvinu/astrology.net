edbd01e7fd580e4207289e8111fcd4df6f0993a5
parabvinu <70007887+parabvinu@users.noreply.github.com>
2023-10-17 10:09:17 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SwissEphNet;
 using CosineKitty;
+using TA;
 
 
 namespace webapp.Controllers
 {
     public class HomeController : Controller
     {
+      
         public ActionResult Index()
         {
             SwissEph sweph = new SwissEph();
-          
-            ViewBag.path = sweph.swe_get_library_path();
+            sweph.swe_set_sid_mode(1,  0,  0);
+            // ViewBag.path = sweph.swe_get_library_path();
             DateTime currentDate = DateTime.UtcNow; // Use UTC time to avoid time zone issues
             int year = DateTime.UtcNow.Year;
             int month = DateTime.UtcNow.Month;
@@ -26,13 +28,25 @@ namespace webapp.Controllers
             double secs = DateTime.UtcNow.Second;
             double latitude = 19.0760;
             double longitude = 72.8777;
+            string estwest = (longitude > 0 ? "E" : "W");
+            string northsouth = (latitude > 0 ? "N" : "S");
+            double[] cusps = new double[13];
+            double[] ascmc = new double[10];
+
             DateTime today = DateTime.Now;
             long julianDate = ConvertToJulian(today);
 
             double hrs = hour + (min / 60) + (secs / 3600);
             double jdt = sweph.swe_julday(year,  month,  day, hrs, 1);
+            double ayanut = sweph.swe_get_ayanamsa_ut(jdt);
+
+            int flags1 = 65536;
+            int results = sweph.swe_houses(jdt, latitude, longitude,  'P',  cusps,  ascmc);
+            flags1 = 2|65536|64|256;
+
+            
             //DateTime normalDateTime = new DateTime(year, month, day, hour, min, sec);
-            //double julianDate = GetJulianDate(normalDateTime);
+            //double jdt = GetJulianDate(today);
 
             // sweph.swe_jdet_to_utc( SwissEph.SE_GREG_CAL,1,ref year,ref month, ref day, ref hour, ref min, ref sec);
             //double jdUTC = sweph.swe_utc_time(jdET, -1);
@@ -43,13 +57,17 @@ namespace webapp.Controllers
             string[] planet = { "रवी", "चन्द्र", "बुध", "शुक्र", "मंगळ", "गुरु", "शनी", "हर्षल", "नेपच्युन", "प्लूटो", "राहू", "केतू" };
             string[] nakshatra = new string[12];
             string[] lord = new string[12];
+            int[] sublords = new int[12];
+
             string[] rashi = {"मेष","वृषभ","मिथुन","कर्क","सिंह","कन्या","तूळ","वृश्चिक","धनु","मकर","कुंभ","मीन"};
             int[] pad = new int[12];
             int nIndx = 0;
             double[] degree = new double[12];
             string serr = string.Empty;
             double[] positions = new double[23];
-            int flags = SwissEph.SEFLG_SPEED;
+            string[] retro = new string[23];
+
+            int flags = 0;
             flags = SwissEph.SEFLG_SPEED | SwissEph.SEFLG_SWIEPH;
             int flag2 = SwissEph.SEFLG_SIDEREAL | SwissEph.SE_SIDM_FAGAN_BRADLEY;
             int result = 0;
@@ -58,55 +76,83 @@ namespace webapp.Controllers
             string[] nklord = { "केतू", "शुक्र", "रवी", "चन्द्र", "मंगळ", "राहू", "गुरु", "शनी", "बुध" };
             string[] nakshatras = { "अश्विनी", "भरणी", "कृत्तिका", "रोहिणी", "मृगशीर्ष", "आर्द्रा", "पुनर्वसू", "पुष्य", "आश्लेषा", "मघा", "पूर्वाफाल्गुनी", "उत्तराफाल्गुनी", "हस्त", "चित्रा", "स्वाती", "विशाखा", "अनुराधा", "ज्येष्ठा", "मूळ", "पूर्वाषाढा", "उत्तराषाढा", "श्रवण", "धनिष्ठा", "शततारका", "पूर्वाभाद्रपदा", "उत्तराभाद्रपदा", "रेवती" };
             int res = 0;
-            double[] cusps = new double[12];
-            double[] ascmc = new double[12];
+            int sublord = 0;
+           
+            //double[] cusps = new double[13];
+            //double[] ascmc = new double[10];
             for (int i = 0; i < 12; i++)
             {
                 if (i == 11)
                 {// for ketu
-                    result = sweph.swe_calc(jdt, 10, flags, positions, ref serr);
-                    //res = sweph.swe_houses(jdt, latitude, longitude, 'P', cusps,  ascmc);
+                    result = sweph.swe_calc_ut(jdt, 10, flags1, positions, ref serr);
                     if (positions[0] < 180)
                     {
                         positions[0] = 180 - positions[0];
                     }
                     if (positions[0] > 180)
                     {
-                        positions[0] =  positions[0] - 180;
+                        positions[0] = positions[0] - 180;
                     }
                     if (positions[0] == 180)
                     {
                         positions[0] = 0;
                     }
-                    nIndx = (int)(positions[0] / 13.33333333);
+                    //nIndx = (int)(positions[0] / 13.33333333);
+                    nIndx = (int)((positions[0]) / 13.33333);
                     nakshatra[i] = nakshatras[nIndx];
                     pad[i] = (int)(positions[0] % 4) + 1;
-                    degree[i] = (positions[0] % 30);
+                    degree[i] = (positions[0]%30);
                     lord[i] = nklord[nIndx % 9];
-                    pos =  (int)(positions[0] / 30);
+                    pos = (int)(Math.Ceiling(positions[0] / 30));
                     rashi[i] = rashi[pos];
+                    if (positions[3] < 0)
+                    {
+                        retro[i] = "R";
+                    }
+                    else
+                    {
+                        retro[i] = "D";
+                    }
+
                     break;
                 }
-                result = sweph.swe_calc(jdt, i, flags, positions, ref serr);
-                //res = sweph.swe_houses(jdt, latitude, longitude, 'P', cusps, ascmc);
-                nIndx = (int)(positions[0] / 13.33333333);
+                result = sweph.swe_calc_ut(jdt, i, flags1, positions, ref serr);
+                
+                //nIndx = (int)(positions[0] / 13.33333333);
+                nIndx = (int)((positions[0])/13.33333);
                 nakshatra[i] = nakshatras[nIndx];
                 pad[i] = (int)(positions[0] % 4) + 1;
-                degree[i] = (positions[0] % 30);
+                degree[i] = (positions[0]%30 );
                 lord[i] = nklord[nIndx % 9];
                 pos =  (int)(positions[0] / 30);
                 rashi[i] = rashi[pos];
-            }
+                if (positions[3] < 0)
+                {
+                    retro[i] = "R";
 
+                }
+                else
+                {
+                    retro[i] = "D";
+                }
+
+                //sublords[i] = CalculateSublord(positions[0]);
+
+            }
+            res = sweph.swe_houses(jdt, latitude, longitude, 'P', cusps, ascmc);
             if (result >= 0)
             {
+                result = sweph.swe_calc(2450009.5, 1, flags, positions, ref serr);
+                ViewBag.Moon = positions;
                 ViewBag.Planet = planet;
                 ViewBag.Nakshtra = nakshatra;
                 ViewBag.Pada = pad;
                 ViewBag.Degree = degree;
                 ViewBag.NakLord = lord;
                 ViewBag.Rashi = rashi;
-
+                //ViewBag.Subloard = sublords;
+                ViewBag.Cusps = cusps;
+                ViewBag.Retro = retro;
             }
             else
             {
@@ -249,9 +295,23 @@ namespace webapp.Controllers
         }
         public ActionResult Data()
         {
+            AstroTime dt = null;
+            DateTime date = new DateTime(1994, 04, 28, 4, 25,0);
+            bool tt = CosineKitty.AstroTime.TryParse(date.ToString(), out dt);
+            ViewBag.date = dt;
+           // CosineKitty.Astronomy.BaryState(Body.Sun, AstroTime date);
+             //CosineKitty.StateVector BaryState(Body.Moon, AstroTime time);
+
             return View();
         }
+        //public static string[] GetGana(string nakshtra)
+        //{
+        //    string[] info;
 
+            
+
+        //    return info;
+        //}
         private static readonly int[] CuspSequence = new int[] { 1, 6, 11, 4, 9, 2, 7, 12, 5, 10, 3, 8 }; // Cusp sequence in KP astrology
 
         private static readonly int[] SublordSequence = new int[] { 2, 7, 12, 5, 10, 3, 8, 1, 6, 11, 4, 9 }; // Sublord sequence in KP astrology
